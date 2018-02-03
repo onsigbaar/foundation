1643fe26038af9a8fc3ce42651db9162f9a2e767
anonymoussc <50c5ac69@opayq.com>
2018-02-03 20:37:03 +0700

    New ErrorLog Onsigbaar\Foundation\Base\Traits

---
+++ src/Base/Traits/ErrorLog.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * ErrorLog.php
+ * Created by @anonymoussc on 01/28/2018 3:57 AM.
+ */
+
+namespace Onsigbaar\Foundation\Base\Traits;
+
+use Illuminate\Support\Facades\Log;
+
+trait ErrorLog
+{
+    public function errorLog($error)
+    {
+        Log::error($error->getMessage());
+        Log::error($error->getCode());
+        Log::error($error->getFile());
+        Log::error($error->getLine());
+        Log::error($error->getTraceAsString());
+    }
+}
